2c7d6caa95b4b49ee2e9a45cf435a1de24446be3
Rob Norris <robn@despairlabs.com>
2025-11-08 15:04:51 +1100

    libzpool: add BE_POSIX_VENDOR for userspace bootenv
    This is mostly a placeholder; it's not actually clear if a boot
    environment makes any sense for userspace. Still, "posix" is the likely
    future name of libzpool as a port, and this define is mandatory, so lets
    roll with it for now.
    
    Sponsored-by: https://despairlabs.com/sponsor/
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Rob Norris <robn@despairlabs.com>
    Closes #17861

--- include/sys/zfs_bootenv.h
+++ include/sys/zfs_bootenv.h
@@ -31,6 +31,7 @@ extern "C" {
 #define	BE_FREEBSD_VENDOR	"freebsd"
 #define	BE_GRUB_VENDOR		"grub"
 #define	BE_LINUX_VENDOR		"linux"
+#define	BE_POSIX_VENDOR		"posix"
 #define	BE_MACOS_VENDOR		"macos"
 
 #include <sys/zfs_bootenv_os.h>
